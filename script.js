daedd4b5377f02095c3a93630171bba8d3e807c8
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-19 00:47:58 -0500

    css

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function validarform(){
-    let nombre=document.getElementById('nombre').value.trim();
-    let email=document.getElementById('email').value.trim();
-    let imagen=document.getElementById('imagen').value();
-    if(nombre=="" || email==""|| imagen==""){
-        alert("Por favor, ingrese todos los datos correspondientes. Son obligatorios");
-        return false;
-    }
-    return true;
-};
-
+const form = document.getElementById('formulario');
+form.addEventListener('submit', function(event) {
+ const nombre = document.getElementById('nombre').value;
+ const email = document.getElementById('email').value;
+ const imagen=document.getElementById('imagen').value();
+ if (nombre=="" || email==""|| imagen=="") {
+ alert('Por favor, ingrese todos los datos correspondientes. Son obligatorios');
+ event.preventDefault();
+ } else {
+ alert('¡Formulario correcto!');
+ }
+})
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
