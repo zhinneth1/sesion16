6fb810ea002bcf79109110982d938aa66ac4c603
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-19 01:08:38 -0500

    css

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ form.addEventListener('submit', function(event) {
  const nombre = document.getElementById('nombre').value;
  const email = document.getElementById('email').value;
  const imagen=document.getElementById('imagen').value();
- if (nombre=="" || email==""|| imagen=="") {
+ const password=document.getElementById('imagen').value().trim();
+ if (nombre=="" || email==""|| imagen=="" || password=="") {
  alert('Por favor, ingrese todos los datos correspondientes. Son obligatorios');
  event.preventDefault();
  } else {
