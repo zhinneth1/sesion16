21e7165790892a5d8f952c08130dfecf4b303503
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-19 00:38:48 -0500

    css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validarform(){
-    let nombre=document.getElementById("nombre").value.trim();
-    let email=document.getElementById("email").value.trim();
-    let imagen=document.getElementById("imagen").value();
+    let nombre=document.getElementById('nombre').value.trim();
+    let email=document.getElementById('email').value.trim();
+    let imagen=document.getElementById('imagen').value();
     if(nombre=="" || email==""|| imagen==""){
         alert("Por favor, ingrese todos los datos correspondientes. Son obligatorios");
         return false;
