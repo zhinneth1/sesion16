f097f5c8fddd03db3f94691712079b11fb427473
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-18 20:35:41 -0500

    css

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.getElementById('formulario').addEventListener('submit', function(event){
+    const nombre=document.getElementById('nombre').Value.trim();
+    const correo=document.getElementById('nombre').Value.trim();
+    const imagen=document.getElementById('nombre').Value.trim();
+});
+
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
