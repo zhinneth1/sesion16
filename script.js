1a55d7fbef0cc06c95fc3bf3b1926faa9cec595b
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-18 22:00:43 -0500

    css

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.getElementById('formulario').addEventListener('submit', function(event){
     let nombre=document.getElementById('nombre').value.trim();
     let correoelectronico=document.getElementById('correo').value.trim();
-    let imagen=document.getElementById('imagen').value().trim();
+    let imagen=document.getElementById('imagen').value();
     let pass=document.getElementById('pass').value().trim();
 
     if(!nombre=="" || !correoelectronico==""|| !imagen=="" || !pass==""){
         alert('Por favor, ingrese todos los datos correspondientes. Son obligatorios');
-        event.preventDefault();
-        return;
+        return false;
     }
 });
 
